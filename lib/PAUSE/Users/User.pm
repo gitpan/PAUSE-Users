c3b1c86b324f00ecdae51e721a66afe92978f574
Neil Bowers <neil@bowers.com>
2013-11-17 22:39:39 +0000

    Import of NEILB/PAUSE-Users-0.01 from CPAN.
    gitpan-cpan-distribution: PAUSE-Users
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         NEILB/PAUSE-Users-0.01.tar.gz
    gitpan-cpan-author:       NEILB
    gitpan-cpan-maturity:     released

---
+++ lib/PAUSE/Users/User.pm
@@ -0,0 +1,19 @@
+package PAUSE::Users::User;
+{
+  $PAUSE::Users::User::VERSION = '0.01';
+}
+
+use 5.10.0;
+use Moo;
+
+has 'asciiname'     => (is => 'ro');
+has 'email'         => (is => 'ro');
+has 'fullname'      => (is => 'ro');
+has 'has_cpandir'   => (is => 'ro');
+has 'homepage'      => (is => 'ro');
+has 'id'            => (is => 'ro');
+has 'info'          => (is => 'ro');
+has 'introduced'    => (is => 'ro');
+has 'type'          => (is => 'ro');
+
+1;
