0a84b87dd67c25b95b894fcbb2be8fcf9a446e0b
Neil Bowers <neil@bowers.com>
2013-11-19 00:34:02 +0000

    Import of NEILB/PAUSE-Users-0.02 from CPAN.
    gitpan-cpan-distribution: PAUSE-Users
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         NEILB/PAUSE-Users-0.02.tar.gz
    gitpan-cpan-author:       NEILB
    gitpan-cpan-maturity:     released

--- lib/PAUSE/Users/User.pm
+++ lib/PAUSE/Users/User.pm
@@ -1,6 +1,6 @@
 package PAUSE::Users::User;
 {
-  $PAUSE::Users::User::VERSION = '0.01';
+  $PAUSE::Users::User::VERSION = '0.02';
 }
 
 use 5.10.0;
@@ -9,11 +9,9 @@ use Moo;
 has 'asciiname'     => (is => 'ro');
 has 'email'         => (is => 'ro');
 has 'fullname'      => (is => 'ro');
-has 'has_cpandir'   => (is => 'ro');
+has 'has_cpandir'   => (is => 'ro', default => sub { 0 } );
 has 'homepage'      => (is => 'ro');
 has 'id'            => (is => 'ro');
-has 'info'          => (is => 'ro');
 has 'introduced'    => (is => 'ro');
-has 'type'          => (is => 'ro');
 
 1;
--- lib/PAUSE/Users/UserIterator.pm
+++ lib/PAUSE/Users/UserIterator.pm
@@ -1,6 +1,6 @@
 package PAUSE::Users::UserIterator;
 {
-  $PAUSE::Users::UserIterator::VERSION = '0.01';
+  $PAUSE::Users::UserIterator::VERSION = '0.02';
 }
 
 use strict;
@@ -39,7 +39,16 @@ sub next_user
         next LINE unless $inuser;
 
         if (m!<([a-zA-Z0-6_]+)>(.*?)</\1>!) {
-            push(@fields, $1 => $2);
+            my ($field, $value) = ($1, $2);
+
+            # <type>author</type> specified a user account
+            # <type>list</type> is a mailing list; we skip those
+            if ($field eq 'type') {
+                $inuser = 0 if $value eq 'list';
+                next LINE;
+            }
+
+            push(@fields, $field => $value);
         }
 
         if (m!</cpanid>!) {
